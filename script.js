5c03458b811de1e6df78fc0d617e9a7520abbddb
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-11 08:13:58 +0300

    Done with question 4

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// Good Luck 💪🏾
+
+
+function calculateTotalPrice(groceries) {
+    let totalPrice = 0;
+  
+    for (let i = 0; i < groceries.length; i++) {
+      let currentGrocery = groceries[i];
+      let quantity = currentGrocery.quantity;
+      let price = currentGrocery.price;
+      let itemTotal = quantity * price;
+  
+      totalPrice += itemTotal;
+    }
+  
+    return totalPrice;
+  }
+
+  const groceries = [
+    {
+      "product": "Milk",
+      "quantity": 2,
+      "price": 1.50
+    },
+    {
+      "product": "bread",
+      "quantity": 1,
+      "price": 3.50
+    },
+    {
+      "product": "jam",
+      "quantity": 2,
+      "price": 2.50
+    },
+    {
+      "product": "butter",
+      "quantity": 3,
+      "price": 4
+    }
+  ];
+  
+  console.log(calculateTotalPrice(groceries));
